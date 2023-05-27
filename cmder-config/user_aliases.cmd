2438bfa85ab987ea2902922e9af57f209c05330a
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2023-05-27 12:31:57 +0200

    chore: add cmder config files

---
+++ cmder-config/user_aliases.cmd
@@ -0,0 +1,16 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+pwd=cd
+clear=cls
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+vim=nvim $*  
+ls=exa $*  
+sudo=runas /noprofile /user:Administrator $*  
