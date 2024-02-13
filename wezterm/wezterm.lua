8125352a0ae573bc4070bfe0e101eef3619ed335
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-13 21:45:58 +0100

    chore(wezterm): use cmd.exe directly with clink

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.default_prog = { 'cmd.exe', '/k', '%CMDER_ROOT%\\vendor\\init.bat' }
+config.default_prog = { 'cmd.exe' }
 config.initial_cols = 100
 config.initial_rows = 24
 config.tab_bar_at_bottom = true
